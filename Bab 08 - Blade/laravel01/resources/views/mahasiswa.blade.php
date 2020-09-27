7a5dd4d9b69d7850e519cd8602d4a2edcef7ffa0
Pri Anton Subardio <prianthon@MacBook-Air-Pri.local>
2020-09-27 16:30:53 +0700

    perulangan for

--- Bab 08 - Blade/laravel01/resources/views/mahasiswa.blade.php
+++ Bab 08 - Blade/laravel01/resources/views/mahasiswa.blade.php
@@ -9,22 +9,11 @@
 </head>
 <body>
   <div class="container text-center mt-3 pt-3 bg-white">
-    <h1 class="bg-dark px-3 py-1 text-white d-inline-block">{!! $nama !!}</h1>
-    <h1 class="bg-dark px-3 py-1 text-white d-inline-block">{!! $nilai !!}</h1>
-    <br>
-    @switch($nilai)
-      @case(0)
-        <div class="alert alert-danger d-inline-block">Tidak ikut ujian</div>
-      @break
-      @case(75)
-        <div class="alert alert-success d-inline-block">Lumayan</div>
-      @break
-      @case(100)
-        <div class="alert alert-success d-inline-block">Sempurna</div>
-      @break
-      @default
-        <div class="alert alert-dark d-inline-block">Nilai tidak valid</div>
-    @endswitch
+    @for($i = 0;$i < 5;$i++)
+    <div class="alert alert-info d-inline-block">
+      Laravel
+    </div>
+    @endfor
   </div>
 </body>
 </html>
