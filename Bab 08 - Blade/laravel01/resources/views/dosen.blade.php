6a605238111ee503853263679840fe376cfe827b
Pri Anton Subardio <prianthon@MacBook-Air-Pri.local>
2020-09-28 11:12:27 +0700

    merancang layout

--- Bab 08 - Blade/laravel01/resources/views/dosen.blade.php
+++ Bab 08 - Blade/laravel01/resources/views/dosen.blade.php
@@ -5,17 +5,17 @@
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link href="/css/bootstrap.min.css" rel="stylesheet">
-  <title>Data Mahasiswa</title>
+  <title>Data Dosen</title>
 </head>
 <body>
 
   <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
     <ul class="navbar-nav">
       <li class="nav-item">
-        <a class="nav-link active" href="/mahasiswa">Data Mahasiswa</a>
+        <a class="nav-link" href="/mahasiswa">Data Mahasiswa</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="/dosen">Data Dosen</a>
+        <a class="nav-link active" href="/dosen">Data Dosen</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="/gallery">Gallery</a>
@@ -24,11 +24,11 @@
   </nav>
 
   <div class="container text-center mt-3 p-4 bg-white">
-    <h1>Data Mahasiswa</h1>
+    <h1>Data Dosen</h1>
     <div class="row">
       <div class="col-sm-8 col-md-6 m-auto">
         <ol class="list-group">
-          @forelse ($mahasiswa as $val)
+          @forelse ($dosen as $val)
             <li class="list-group-item">{{$val}}</li>
           @empty
             <div class="alert alert-dark d-inline-block">Tidak ada data...</div>
--- Bab 08 - Blade/laravel01/routes/web.php
+++ Bab 08 - Blade/laravel01/routes/web.php
@@ -18,8 +18,17 @@ Route::get('/', function () {
 });
 
 Route::get('/mahasiswa', function() {
-  $nama = 'Tya Kirana Putri';
-  $nilai = [80,64,30,76,95];
+  $arrMahasiswa = ["Risa Lestari","Rudi Hermawan","Bambang Kusumo","Lisa Permata"];
 
-  return view('mahasiswa', compact('nama','nilai'));
+  return view('mahasiswa')->with('mahasiswa',$arrMahasiswa);
+});
+
+Route::get('dosen', function() {
+  $arrDosen = ["Maya Fitrianti, M.M.","Prof. Silvia Nst, M.Farm.","Dr. Umar Agustinus","Dr. Syahrial, M.Kom"];
+
+  return view('dosen')->with('dosen',$arrDosen);
+});
+
+Route::get('gallery', function() {
+  return view('gallery');
 });
