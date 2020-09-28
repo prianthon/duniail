a06836b03adbc6fb4711d64f67dfdda246cb13f0
Pri Anton Subardio <prianthon@MacBook-Air-Pri.local>
2020-09-28 14:40:45 +0700

    component dan slot admin

--- Bab 08 - Blade/laravel01/resources/views/admin.blade.php
+++ Bab 08 - Blade/laravel01/resources/views/admin.blade.php
@@ -7,24 +7,19 @@
   <link href="/css/bootstrap.min.css" rel="stylesheet">
   <title>Admin Dashboard</title>
 </head>
+<body>
 <div class="container text-center mt-3 p-4 bg-white">
   <h1>Halaman Admin</h1>
   <div class="row">
     <div class="col-12">
 
-      <div class="alert alert-warning alert-dismissible fade show">
+      @component('layout.alert')
         100 data mahasiswa perlu di perbaiki
-        <button type="button" class="close" data-dismiss="alert">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
+      @endcomponent
 
-      <div class="alert alert-danger alert-dismissible fade show">
+      @component('layout.alert')
         Hari ini deadline laporan perjalanan dinas!
-        <button type="button" class="close" data-dismiss="alert">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
+      @endcomponent
 
     </div>
   </div>
