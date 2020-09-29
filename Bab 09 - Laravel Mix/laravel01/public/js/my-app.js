67ec672fea148ee2f6fba210706aa88e0969f1ca
Pri Anton Subardio <prianthon@MacBook-Air-Pri.local>
2020-09-29 20:07:11 +0700

    bab9 laravelmix penggabungan file

---
+++ Bab 09 - Laravel Mix/laravel01/public/js/my-app.js
@@ -0,0 +1,11 @@
+// Tampilkan alert ketika halaman di klik
+
+window.addEventListener('click', function() {
+  alert('Saya di klik');
+});
+
+// Tampilkan pesan console ketika halaman di klik
+
+window.addEventListener('click', function() {
+  console.log('Saya juga di klik');
+});
--- Bab 09 - Laravel Mix/laravel01/webpack.mix.js
+++ Bab 09 - Laravel Mix/laravel01/webpack.mix.js
@@ -1,15 +1,11 @@
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+mix.scripts([
+  'resources/js/my-script.js',
+  'resources/js/my-script-console.js',
+],'public/js/my-app.js');
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.styles([
+  'resources/css/my-style.css',
+  'resources/css/my-style-h1.css',
+],'public/css/my-app.css');
