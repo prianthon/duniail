38c9b00a3445ac44ef9fcd6ef16aa8945b1bb956
Pri Anton Subardio <prianthon@MacBook-Air-Pri.local>
2020-09-30 09:37:50 +0700

    bab9 laravelmix sass

--- Bab 09 - Laravel Mix/laravel01/webpack.mix.js
+++ Bab 09 - Laravel Mix/laravel01/webpack.mix.js
@@ -5,7 +5,4 @@ mix.scripts([
   'resources/js/my-script-console.js',
 ],'public/js/my-app.js');
 
-mix.styles([
-  'resources/css/my-style.css',
-  'resources/css/my-style-h1.css',
-],'public/css/my-app.css');
+mix.sass('resources/sass/my-style-h1.scss','public/css/my-app.css');
