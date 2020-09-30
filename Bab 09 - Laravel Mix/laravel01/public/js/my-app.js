f03c6b42d1064994c124ef8ebdb6d34a9301306d
Pri Anton Subardio <prianthon@MacBook-Air-Pri.local>
2020-09-30 10:01:44 +0700

    bab9 laravelmix sass minify

--- Bab 09 - Laravel Mix/laravel01/public/js/my-app.js
+++ Bab 09 - Laravel Mix/laravel01/public/js/my-app.js
@@ -1,11 +1,1 @@
-// Tampilkan alert ketika halaman di klik
-
-window.addEventListener('click', function() {
-  alert('Saya di klik');
-});
-
-// Tampilkan pesan console ketika halaman di klik
-
-window.addEventListener('click', function() {
-  console.log('Saya juga di klik');
-});
+window.addEventListener("click",function(){alert("Saya di klik")}),window.addEventListener("click",function(){console.log("Saya juga di klik")});
