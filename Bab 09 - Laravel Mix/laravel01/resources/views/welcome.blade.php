3051afdc9f5eb3feb2fb97b7e698933fad1548a7
Pri Anton Subardio <prianthon@MacBook-Air-Pri.local>
2020-09-30 10:19:54 +0700

    bab9 laravelmix cache busting

--- Bab 09 - Laravel Mix/laravel01/resources/views/welcome.blade.php
+++ Bab 09 - Laravel Mix/laravel01/resources/views/welcome.blade.php
@@ -5,10 +5,10 @@
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Document</title>
-  <link rel="stylesheet" href="{{ asset('/css/my-app.css') }}">
+  <link rel="stylesheet" href="{{ mix('/css/my-app.css') }}">
 </head>
 <body>
   <h1>Belajar Laravel</h1>
-  <script src="{{ asset('/js/my-app.js') }}"></script>
+  <script src="{{ mix('/js/my-app.js') }}"></script>
 </body>
 </html>
