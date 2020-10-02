de5242f2dcffc48ad744099b600265e54adc501b
Pri Anton Subardio <prianthon@MacBook-Air-Pri.local>
2020-10-02 20:20:01 +0700

    bab12 method unique

--- Bab 12 - Collection/laravel01/app/Http/Controllers/CollectionController.php
+++ Bab 12 - Collection/laravel01/app/Http/Controllers/CollectionController.php
@@ -96,9 +96,13 @@ class CollectionController extends Controller
       //echo $collection->concat([10,11,12]); // [1,9,3,4,5,3,5,7,10,11,12]
 
       // Cek apakah angka 3 ada di dalam collection
-      dump( $collection->contains(3) ); // true
+      //dump( $collection->contains(3) ); // true
       // Cek apakah angka 8 ada di dalam collection
-      dump( $collection->contains(8) ); // false
+      //dump( $collection->contains(8) ); // false
+
+      // Filter dan tampilkan element yang tidak berulang (unik)
+      // $collection = collect(["0"=>1,"1"=>9,"2"=>3,"3"=>4,"4"=>5,"6"=>5,"7"=>7]);
+      echo $collection->unique(); // {"0":1,"1":9,"2":3,"3":4,"4":5,"7":7}
     }
 
     public function collectionEmpat()
