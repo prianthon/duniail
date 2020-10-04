aa4bb0b85463e11d55803166606b32ad5f34c24e
Pri Anton Subardio <prianthon@MacBook-Air-Pri.local>
2020-10-04 14:41:13 +0700

    bab12 dump exercise

--- Bab 12 - Collection/laravel01/app/Http/Controllers/CollectionController.php
+++ Bab 12 - Collection/laravel01/app/Http/Controllers/CollectionController.php
@@ -309,4 +309,31 @@ class CollectionController extends Controller
       $namaJurusanIpa = $siswas->groupBy('jurusan')->get('IPA')->pluck('nama')->all();
       echo 'Nama siswa jurusan IPA: '.implode(', ',$namaJurusanIpa);
     }
+
+    public function exercise()
+    {
+      $matkul00 = new \stdClass();
+      $matkul00->namaMatkul = "Sistem Operasi";
+      $matkul00->jumlahSks = 3;
+      $matkul00->semester = 3;
+
+      $matkul01 = new \stdClass();
+      $matkul01->namaMatkul = "Algoritma dan Pemrograman";
+      $matkul01->jumlahSks = 4;
+      $matkul01->semester = 1;
+
+      $matkul02 = new \stdClass();
+      $matkul02->namaMatkul = "Kalkulus Dasar";
+      $matkul02->jumlahSks = 2;
+      $matkul02->semester = 1;
+
+      $matkul03 = new \stdClass();
+      $matkul03->namaMatkul = "Basis Data";
+      $matkul03->jumlahSks = 2;
+      $matkul03->semester = 3;
+
+      $matkuls = collect([$matkul00, $matkul01, $matkul02, $matkul03]);
+
+      dump($matkuls);
+    }
 }
