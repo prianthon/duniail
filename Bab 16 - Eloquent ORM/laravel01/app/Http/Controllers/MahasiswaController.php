b3e279db20f6c8d50280186238fb344eb417db29
Pri Anton Subardio <prianthon@MacBook-Air-Pri.local>
2020-10-08 14:16:12 +0700

    create mass

--- Bab 16 - Eloquent ORM/laravel01/app/Http/Controllers/MahasiswaController.php
+++ Bab 16 - Eloquent ORM/laravel01/app/Http/Controllers/MahasiswaController.php
@@ -3,9 +3,69 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Mahasiswa;
 
 class MahasiswaController extends Controller
 {
-    // kode program akan kita tulis disini
+    public function cekObject(){
+      $mahasiswa = new Mahasiswa;
 
+      dump($mahasiswa);
+    }
+
+    public function insert(){
+      $mahasiswa = new Mahasiswa;
+      $mahasiswa->nim = '19003036';
+      $mahasiswa->nama = 'Sari Citra Lestari';
+      $mahasiswa->tanggal_lahir = '2001-12-31';
+      $mahasiswa->ipk = 3.5;
+      $mahasiswa->save();
+
+      dump($mahasiswa);
+    }
+
+    public function massAssignment(){
+      Mahasiswa::create(
+        [
+          'nim' => '19021044',
+          'nama' => 'Rudi Permana',
+          'tanggal_lahir' => '2000-08-22',
+          'ipk' => 2.5,
+        ]
+      );
+
+      return "Berhasil di proses";
+    }
+
+    public function massAssignment2(){
+      $mahasiswa1 = Mahasiswa::create(
+        [
+          'nim' => '19002032',
+          'nama' => 'Rina Kumala Sari',
+          'tanggal_lahir' => '2000-06-28',
+          'ipk' => 3.4,
+        ]
+      );
+      dump($mahasiswa1);
+
+      $mahasiswa2 = Mahasiswa::create(
+        [
+          'nim' => '18012012',
+          'nama' => 'James Situmorang',
+          'tanggal_lahir' => '1999-04-02',
+          'ipk' => 2.7,
+        ]
+      );
+      dump($mahasiswa2);
+
+      $mahasiswa3 = Mahasiswa::create(
+        [
+          'nim' => '19005011',
+          'nama' => 'Riana Putria',
+          'tanggal_lahir' => '2000-11-23',
+          'ipk' => 2.9,
+        ]
+      );
+      dump($mahasiswa3);
+    }
 }
