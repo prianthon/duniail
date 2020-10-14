5f00b321a3a806f015ab84ab0f4275ef758488d2
prianthon <prianthonsubardio@gmail.com>
2020-10-14 14:01:32 +0700

    20 file upload

--- Bab 20 - File Upload/laravel01/app/Http/Controllers/FileUploadController.php
+++ Bab 20 - File Upload/laravel01/app/Http/Controllers/FileUploadController.php
@@ -6,5 +6,42 @@ use Illuminate\Http\Request;
 
 class FileUploadController extends Controller
 {
-    //
+    public function fileUpload(){
+      return view('file-upload');
+    }
+
+    public function prosesFileUpload(Request $request){
+      //dump($request->berkas);
+
+      //if($request->hasFile('berkas'))
+      //{
+      //  echo "path(): ".$request->berkas->path();
+      //  echo "<br>";
+      //  echo "extension(): ".$request->berkas->extension();
+      //  echo "<br>";
+      //  echo "getClientOriginalExtension(): ".$request->berkas->getClientOriginalExtension();
+      //  echo "<br>";
+      //  echo "getMimeType(): ".$request->berkas->getMimeType();
+      //  echo "<br>";
+      //  echo "getClientOriginalName(): ".$request->berkas->getClientOriginalName();
+      //  echo "<br>";
+      //  echo "getSize(): ".$request->berkas->getSize();
+      //}
+      //else
+      //{
+      //  echo "Tidak ada berkas yang diupload";
+      //}
+
+      // Validasi required
+      //$request->validate([
+      //  'berkas' => 'required',
+      //]);
+
+      // Validasi tipe data dan maksimum file
+      $request->validate([
+        'berkas' => 'required|file|image|max:1000',
+      ]);
+
+      echo $request->berkas->getClientOriginalName()."Lolos Validasi";
+    }
 }
