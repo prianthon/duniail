37527dd5e910f7f9850e1040d8f7f03e9871a132
prianthon <prianthonsubardio@gmail.com>
2020-10-14 12:17:35 +0700

    19 selesai

--- Bab 19 - CRUD/laravel01/routes/web.php
+++ Bab 19 - CRUD/laravel01/routes/web.php
@@ -13,16 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/mahasiswas','MahasiswaController@index')->name('mahasiswas.index');
-
-Route::get('/mahasiswas/create','MahasiswaController@create')->name('mahasiswas.create');
-
-Route::post('/mahasiswas','MahasiswaController@store')->name('mahasiswas.store');
-
-Route::get('/mahasiswas/{mahasiswa}','MahasiswaController@show')->name('mahasiswas.show');
-
-Route::get('/mahasiswas/{mahasiswa}/edit','MahasiswaController@edit')->name('mahasiswas.edit');
-
-Route::patch('/mahasiswas/{mahasiswa}','MahasiswaController@update')->name('mahasiswas.update');
-
-Route::delete('/mahasiswa/{mahasiswa}','MahasiswaController@destroy')->name('mahasiswas.destroy');
+Route::resource('mahasiswas','MahasiswaController');
