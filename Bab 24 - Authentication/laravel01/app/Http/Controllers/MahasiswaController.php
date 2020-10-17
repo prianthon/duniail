0c8565a1effaf40a32370535a2b44ebb474e1de4
prianthon <prianthonsubardio@gmail.com>
2020-10-17 10:48:50 +0700

    menampilkan data user

--- Bab 24 - Authentication/laravel01/app/Http/Controllers/MahasiswaController.php
+++ Bab 24 - Authentication/laravel01/app/Http/Controllers/MahasiswaController.php
@@ -3,12 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class MahasiswaController extends Controller
 {
-    public function daftarMahasiswa()
+    public function daftarMahasiswa(Request $request)
     {
-      return view('halaman',['judul'=>'Daftar Mahasiswa']);
+      //return view('halaman',['judul'=>'Daftar Mahasiswa']);
+
+      // Lihat isi dari User object
+      // Test ambil beberapa property dari Auth object menggunakan Auth facades
+      //echo Auth::user()->id."<br>";
+      //echo Auth::user()->name."<br>";
+      //echo Auth::user()->email."<br>";
+      //echo Auth::user()->password."<br>";
+
+      //dump(Auth::user());
+
+      // Test ambil beberapa property dari Auth object, ini menggunakan $request
+      //echo $request->user()->id."<br>";
+      //echo $request->user()->name."<br>";
+      //echo $request->user()->email."<br>";
+      //echo $request->user()->password."<br>";
+
+      // Cek apakah user sudah login atau tidak
+      if(Auth::check())
+      {
+        echo "Selamat datang, ".$request->user()->name;
+      }
+      else
+      {
+        echo "Silahkan login terlebih dahulu";
+      }
     }
 
     public function tabelMahasiswa()
