8976aa61816b119b884210fce34c4481b8e6572b
prianthon <prianthonsubardio@gmail.com>
2020-10-19 10:12:46 +0700

    27 modifikasi view layout

--- Bab 27 - Case Study ILKOOM Profile Manager/laravel01/resources/views/layouts/app.blade.php
+++ Bab 27 - Case Study ILKOOM Profile Manager/laravel01/resources/views/layouts/app.blade.php
@@ -1,80 +1,152 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_','-', app()->getLocale()) }}">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+  <title>ILKOOM - Community Blog</title>
+  <link rel="icon" href="{{ asset('img/favicon.png') }}" type="image/png">
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+  <!-- Scripts -->
+  <script src="{{ asset('js/app.js') }}" defer></script>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <!-- Styles -->
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+  <!-- NAVBAR -->
+  <nav id="main-navbar" class="navbar navbar-expand-md navbar-dark bg-dark py-0">
+    <div class="container">
+      <a class="navbar-brand" href="{{ route('home') }}">
+        <span class="d-none">ILKOOM</span>
+        <img src="{{ asset('img/ilkoom_logo.png') }}" class="main-logo d-none d-md-inline" alt="ilkoom logo">
+        <img src="{{ asset('img/ilkoom_logo.png') }}" class="small-logo d-inline d-md-none" alt="ilkoom logo">
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+      <div class="collapse navbar-collapse" id="navbarNav">
 
-                    </ul>
+        <!-- Left Side Of Navbar -->
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link p-4 active" href="{{ route('home') }}">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link p-4" href="#">Blog</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link p-4" href="#">Article</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link p-4" href="#">Gallery</a>
+          </li>
+        </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
+        <!-- Right Side Of Navbar -->
+        <ul class="navbar-nav ml-auto">
+          <!-- Authentication Links -->
+          @guest
+            <li class="nav-item">
+              <a class="nav-link p-4" href="{{ route('login') }}">Login</a>
+            </li>
+            @if(Route::has('register'))
+              <li class="nav-item">
+                <a class="nav-link p-4" href="{{ route('register') }}">Register</a>
+              </li>
+            @endif
+          @else
+            <li class="nav-item dropdown">
+              <a id="navbarDropdown" class="nav-link dropdown-toggle p-4" href="#" role="button" data-toggle="dropdown" v-pre>
+                {{ Auth::user()->nama }} <span class="caret"></span>
+              </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+              <div class="dropdown-menu dropdown-menu-right p-0 border-0">
+                <a class="dropdown-item bg-dark text-white p-2 text-center" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                  @csrf
+                </form>
+              </div>
+            </li>
+          @endguest
+        </ul>
+      </div>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
     </div>
+  </nav>
+
+  @yield('content')
+
+  <!-- FOOTER -->
+  <footer id="main-footer" class="text-white bg-dark py-4">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3 text-center text-md-left">
+          <a href="index.html">
+            <img src="{{ asset('img/ilkoom_logo.png') }}" style="height: 60px;">
+          </a>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam cumque, esse modi maxime veniam nulla delectus dolorem</p>
+        </div>
+
+        <div class="col-md-3 text-center">
+          <h5>Community</h5>
+          <ul class="list-unstyled">
+            <li><a href="#" class="text-white">Activity</a></li>
+            <li><a href="#" class="text-white">Groups</a></li>
+            <li><a href="#" class="text-white">Forums</a></li>
+          </ul>
+        </div>
+
+        <div class="col-md-3 text-center">
+          <h5>Our Services</h5>
+          <ul class="list-unstyled">
+            <li><a href="#" class="text-white">Our mission</a></li>
+            <li><a href="#" class="text-white">Help/Contact Us</a></li>
+            <li><a href="#" class="text-white">Privacy Policy</a></li>
+            <li><a href="#" class="text-white">Cookie Policy</a></li>
+            <li><a href="#" class="text-white">Terms & Conditions</a></li>
+          </ul>
+        </div>
+
+        <div class="col-md-3 text-center text-md-left">
+          <h5>Hubungi Kami</h5>
+          <div class="text-nowrap"><i class="fas fa-envelope fa-fw mr-3"></i>andre@ilkoom.com</div>
+          <div class="text-nowrap"><i class="fas fa-phone fa-fw mr-3"></i>(021) 12345</div>
+          <div class="text-nowrap"><i class="fas fa-globe fa-fw mr-3"></i>www.ilkoom.com</div>
+        </div>
+      </div>
+
+      <div class="row mt-3 mt-md-0">
+        <div class="col-md-3 mr-md-auto text-center text-md-left">
+          <small>&copy; ILKOOM 2018</small>
+        </div>
+        <div class="col-md-3 text-center text-md-left">
+          <div>
+            <a href="#" class="text-white mr-2">
+              <i class="fab fa-facebook fa-lg"></i>
+            </a>
+            <a href="#" class="text-white mr-2">
+              <i class="fab fa-twitter fa-lg"></i>
+            </a>
+            <a href="#" class="text-white mr-2">
+              <i class="fab fa-instagram fa-lg"></i>
+            </a>
+            <a href="#" class="text-white mr-2">
+              <i class="fab fa-google-plus fa-lg"></i>
+            </a>
+            <a href="#" class="text-white mr-2">
+              <i class="fab fa-github fa-lg"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </footer>
 </body>
 </html>
